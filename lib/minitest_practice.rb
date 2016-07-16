a06e74ea149c0c57cd3cfea07f8b13b25f86b17d
terra_yucco <terashima.yuko@gmail.com>
2016-07-16 08:05:59 +0000

    add 6-4 feature code

--- lib/minitest_practice.rb
+++ lib/minitest_practice.rb
@@ -2,10 +2,13 @@ require "minitest_practice/version"
 
 module MinitestPractice
   class Sample
+
+    # 6-2
     def odd?(val)
       return (val % 2 == 1)
     end
 
+    # 6-3
     def check_number?(val)
       if val.to_s.length != 4
         return false
@@ -14,6 +17,11 @@ module MinitestPractice
         return false
       end
       return !(odd?(val.to_i))
+    end
+
+    # 6-4
+    def enough_length?(val)
+      return (3 <= val.to_s.length && val.to_s.length <= 8)
     end
   end
 end

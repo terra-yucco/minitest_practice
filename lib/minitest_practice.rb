87ec6fa7dd972f9bdc32e14212cb8fc82b697de3
terra_yucco <terashima.yuko@gmail.com>
2016-07-16 07:58:40 +0000

    add feature code

--- lib/minitest_practice.rb
+++ lib/minitest_practice.rb
@@ -5,5 +5,15 @@ module MinitestPractice
     def odd?(val)
       return (val % 2 == 1)
     end
+
+    def check_number?(val)
+      if val.to_s.length != 4
+        return false
+      end
+      if val.to_s[0] == '0'
+        return false
+      end
+      return !(odd?(val.to_i))
+    end
   end
 end

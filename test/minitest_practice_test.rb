614daecc1079e1ee23848dd0e95ecd4beb61a9fc
terra_yucco <terashima.yuko@gmail.com>
2016-07-16 07:35:55 +0000

    add test method

--- test/minitest_practice_test.rb
+++ test/minitest_practice_test.rb
@@ -1,11 +1,17 @@
 require 'test_helper'
 
 class MinitestPracticeTest < Minitest::Test
+  def setup
+    @target = MinitestPractice::Sample
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::MinitestPractice::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_for_odd?
+    assert_equal @target.odd?(0), false
+    assert_equal @target.odd?(1), true
+    assert_equal @target.odd?(2), false
   end
 end
